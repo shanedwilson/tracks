72bc4dd699f8121d121af91c2699871368bd70df
Shane D Wilson <shane@fortyau.com>
2021-10-11 20:40:31 -0500

    chapter 209 complete

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-// import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import AccountScreen from './src/screens/AccountScreen';
-import SigninScreen from './src/screens/SigninScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import TrackCreateScreen from './src/screens/TrackCreateScreen';
-import TrackDetailScreen from './src/screens/TrackDetailScreen';
-import TrackListScreen from './src/screens/TrackListScreen';
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import AccountScreen from './src/screens/AccountScreen'
+import SigninScreen from './src/screens/SigninScreen'
+import SignupScreen from './src/screens/SignupScreen'
+import TrackCreateScreen from './src/screens/TrackCreateScreen'
+import TrackDetailScreen from './src/screens/TrackDetailScreen'
+import TrackListScreen from './src/screens/TrackListScreen'
+import { Provider as AuthProvider } from './src/context/authContext'
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -22,6 +23,14 @@ const switchNavigator = createSwitchNavigator({
     TrackCreate: TrackCreateScreen,
     Account: AccountScreen,
   }),
-});
+})
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator)
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
+  )
+}
