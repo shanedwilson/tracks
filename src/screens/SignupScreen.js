29b5464c2770bc089302dcfc4d26d5c5e35d03c7
Shane D Wilson <shane@fortyau.com>
2021-10-12 00:11:23 -0500

    completed chapter 216

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -31,6 +31,7 @@ const SignupScreen = ({ navigation }) => {
                 autoCorrect={false}
                 secureTextEntry
             />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
             <Spacer>
                 <Button
                     title="Sign Up"
@@ -52,6 +53,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginTop: 15,
+        marginLeft: 15
     }
 })
 
