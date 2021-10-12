8ec6a03f6762f2859054a8baed642e5ac4e595e5
Shane D Wilson <shane@fortyau.com>
2021-10-12 00:12:08 -0500

    chapter 216 complete

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'http://904e-68-52-144-206.ngrok.io'
+})
---
+++ src/context/authContext.js
@@ -0,0 +1,44 @@
+import createDataContext from "./createDataContext"
+import trackerApi from '../api/tracker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { navigate } from '../navigationRef'
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_error':
+            return {...state, errorMessage: action.payload}
+            case 'signup':
+                return {errorMessage: '', token: action.payload}
+        default:
+            return state
+    }
+}
+
+const signup = (dispatch) => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password })
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({ type: 'signup', payload: response.data.token })
+        navigate('TrackList')
+    } catch (err) {
+        dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
+    }
+}
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        
+    }
+}
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    { signup, signin, signout },
+    { token: null, errorMessage: '' }
+)
