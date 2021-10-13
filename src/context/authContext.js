a7a16f318fd5a7266dee319b0af63e9fd4f42eb4
Shane D Wilson <shane@fortyau.com>
2021-10-13 16:35:54 -0500

    completed chapter 229

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -11,8 +11,20 @@ const authReducer = (state, action) => {
             return {errorMessage: '', token: action.payload}
         case 'clear_error_message':
             return { ...state, errorMessage: '' }
+        case 'signout':
+            return { token: null, errorMessage: '' }
         default:
             return state
+    }
+}
+
+const tryLocalSignin = dispatch => async () => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+        dispatch({ type: 'signin', payload: token })
+        navigate('TrackList')
+    } else {
+        navigate('loginFlow')
     }
 }
 
@@ -45,14 +57,14 @@ const signin = dispatch => async ({ email, password }) => {
     }
 }
 
-const signout = (dispatch) => {
-    return () => {
-        
+const signout = (dispatch) => async () => {
+        await AsyncStorage.removeItem('token')
+        dispatch({ type: 'signout'})
+        navigate('loginFlow')
     }
-}
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signup, signin, signout, clearErrorMessage },
+    { signup, signin, signout, clearErrorMessage, tryLocalSignin },
     { token: null, errorMessage: '' }
 )
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/authContext'
 
 const AccountScreen = () => {
+    const { signout } = useContext(AuthContext)
     return (
-        <Text style={{ fontSize: 48 }}>Account Screen</Text>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text style={{ fontSize: 48 }}>Account Screen</Text>
+            <Spacer>
+                <Button
+                    title="Sign Out"
+                    onPress={signout}
+                />
+            </Spacer>
+        </SafeAreaView>
     )
 }
 
