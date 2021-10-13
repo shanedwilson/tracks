5a83dab37c76de6df9fa84f7155ee1347cd6073b
Shane D Wilson <shane@fortyau.com>
2021-10-12 22:52:31 -0500

    completed chapter 225

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -3,12 +3,16 @@ import { View, StyleSheet } from 'react-native'
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
 import { Context } from '../context/authContext'
+import {  NavigationEvents } from 'react-navigation'
 
 const SigninScreen = () => {
-    const { state, signin } = useContext(Context)
+    const { state, signin, clearErrorMessage } = useContext(Context)
 
     return (
         <View style={styles.container}>
+            <NavigationEvents
+                onWillFocus={clearErrorMessage}
+            />
             <AuthForm
                 headerText="Sign In to Your Account"
                 errorMessage={state.errorMessage}
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,12 +3,16 @@ import { View, StyleSheet } from 'react-native'
 import { Context as AuthContext } from '../context/authContext'
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
+import { NavigationEvents } from 'react-navigation'
 
 const SignupScreen = () => {
-    const { state, signup } = useContext(AuthContext)
+    const { state, signup, clearErrorMessage } = useContext(AuthContext)
 
     return (
         <View style={styles.container}>
+            <NavigationEvents
+                onWillFocus={clearErrorMessage}
+            />
             <AuthForm
                 headerText="Sign Up for Tracker"
                 errorMessage={state.errorMessage}
