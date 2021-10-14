f5c1d045be87288ef4ca400f2563d97ff03bb94c
Shane D Wilson <shane@fortyau.com>
2021-10-13 19:44:40 -0500

    chapter 237 complete

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { requestForegroundPermissionsAsync } from 'expo-location'
+import Map from '../components/Map'
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+    
+    const startWatching = async () => {
+        try {
+                const { granted } = await requestForegroundPermissionsAsync();
+                if (!granted) {
+                    throw new Error('Location permission not granted')
+                }
+            } catch (e) {
+                setErr(e)
+            }
+    }
+    
+    useEffect(() => {
+        startWatching()
+    }, [])
+
     return (
-        <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+        <SafeAreaView forceInset={{ top: 'always'}}>
+            <Text h2>Create A Track</Text>
+            <Map/>
+            {err ? <Text>Please enable location services.</Text> : null}
+        </SafeAreaView>
     )
 }
 
